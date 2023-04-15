2b1b02e430842a88fbf5b5ba200805a63ae41ad4
linkq <mrlinkq@hotmail.com>
2023-04-16 01:19:00 +0800

    前端修改，增加用户时直接生成二维码

--- web/xxqg/src/compents/pages/AddUser.tsx
+++ web/xxqg/src/compents/pages/AddUser.tsx
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import {Button, Dialog, Toast} from "antd-mobile";
+import QrCode from "qrcode.react";
 import {checkQrCode, getLink, getToken} from "../../utils/api";
 
 class AddUser extends Component<any, any>{
@@ -33,15 +34,15 @@ class AddUser extends Component<any, any>{
 
     click = async () => {
         console.log(this.isMobile())
-        if (!this.isMobile()){
-            Dialog.show({
-                title:"提醒",
-                content:"网页端不再生成二维码，请在移动端访问该网页，会自动跳转到xxqg",
-                closeOnAction:true,
-                closeOnMaskClick:true,
-            })
-            return
-        }
+        // if (!this.isMobile()){
+        //     Dialog.show({
+        //         title:"提醒",
+        //         content:"网页端不再生成二维码，请在移动端访问该网页，会自动跳转到xxqg",
+        //         closeOnAction:true,
+        //         closeOnMaskClick:true,
+        //     })
+        //     return
+        // }
         let data = await getLink()
 
         this.setState({
@@ -58,7 +59,7 @@ class AddUser extends Component<any, any>{
                 let token = await getToken(resp.data.split("=")[1],data.sign)
                 console.log(token)
                 if (token.success){
-                    Toast.show("登录成功\n该软件为免费软件，若你正在付费使用，请速度举报管理员")
+                    Toast.show("登录成功")
                     this.setState({
                         link : ""
                     })
@@ -83,20 +84,21 @@ class AddUser extends Component<any, any>{
         return <div style={{width:"100%",height:"50%"}}>
             <h2 style={{margin:10,color:"red",display:this.isWechat()}}>当前环境为微信环境，请点击右上角在浏览器中打开</h2>
             <Button onClick={this.click} color={"primary"} style={{marginRight:10,marginTop:10,marginBottom:10}} block>生成链接</Button>
-            <span>{this.state.link}</span>
-            {/*<QRCode*/}
-            {/*    id="qrCode"*/}
-            {/*    value={this.state.img}*/}
-            {/*    size={400} // 二维码的大小*/}
-            {/*    fgColor="#000000" // 二维码的颜色*/}
-            {/*    style={{ margin: 'auto' ,display:this.state.img === "你还未获取登录链接" ? "none" : "block"}}*/}
-            {/*    imageSettings={{ // 二维码中间的logo图片*/}
-            {/*        src: qr,*/}
-            {/*        height: 100,*/}
-            {/*        width: 100,*/}
-            {/*        excavate: true, // 中间图片所在的位置是否镂空*/}
-            {/*    }}*/}
-            {/*/>*/}
+            {/* <span>{this.state.link}</span> */}
+            <QrCode
+                id="qrCode"
+                value={this.state.img}
+                size={400} // 二维码的大小
+                fgColor="#000000" // 二维码的颜色
+                style={{ margin: 'auto' ,display:this.state.img === "你还未获取登录链接" ? "none" : "block"}}
+                imageSettings={{ // 二维码中间的logo图片
+                    src: "qr",
+                    height: 100,
+                    width: 100,
+                    excavate: true, // 中间图片所在的位置是否镂空
+                }}
+            />
+            {/* <QrCode style={{marginLeft:10,display:this.state.img === "你还未获取登录链接" ? "none":"block"}} fgColor={"#000000"} size={200} value={this.state.img} /> */}
         </div>;
     }
 }
