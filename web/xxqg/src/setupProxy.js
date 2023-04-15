31da9508f6676583c47c271d1adc7108b46946a1
linkq <mrlinkq@hotmail.com>
2023-04-16 01:20:43 +0800

    修改web页面开发时后端端口为8080

--- web/xxqg/src/setupProxy.js
+++ web/xxqg/src/setupProxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
  module.exports = function(app) {
     app.use("/api",createProxyMiddleware({
-        target: "http://127.0.0.1:8085",
+        target: "http://127.0.0.1:8080",
         changeOrigin: true,
         pathRewrite:{
             "/api":"/"
